ad9728255b045223555d701af9baef62d9e21cb8
Andrew Kvapil <git@viluon.me>
2025-01-02 23:26:24 +0100

    Add Node dependency

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 # do not install dependencies here
 # APT & other setup commands belong in the Dockerfile
 
 gzip --version
-node --version
+echo | node --interactive
 
 latexmk -pdf -file-line-error cv.tex
